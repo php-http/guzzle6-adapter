d940ae4db71d4e9317369993860dfc2bf78c2c1b
Joel Wurtz <jwurtz@jolicode.com>
2015-10-23 23:58:38 +0200

    Update guzzle6 adapater with httplug new contract

--- src/Guzzle6HttpAdapter.php
+++ src/Guzzle6HttpAdapter.php
@@ -15,13 +15,18 @@ use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Pool;
+use Http\Client\Exception\BatchException;
+use Http\Client\Exception\HttpException;
+use Http\Client\Exception\NetworkException;
+use Http\Client\HttpClient;
+use Http\Client\Utils\BatchResult;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 /**
  * @author David de Boer <david@ddeboer.nl>
  */
-class Guzzle6HttpAdapter implements HttpAdapter
+class Guzzle6HttpAdapter implements HttpClient
 {
     /**
      * @var ClientInterface
@@ -29,22 +34,27 @@ class Guzzle6HttpAdapter implements HttpAdapter
     private $client;
 
     /**
-     * @param ClientInterface|null $client
+     * @var array Options to pass when sending one or multiple requests with guzzle
      */
-    public function __construct(ClientInterface $client = null)
+    private $options;
+
+    /**
+     * @param ClientInterface|null $client  Guzzle client
+     * @param array                $options Options to pass when sending one or multiple requests with guzzle
+     */
+    public function __construct(ClientInterface $client = null, array $options = [])
     {
-        $this->client = $client ?: new Client();
+        $this->client  = $client ?: new Client();
+        $this->options = $this->buildOptions($options);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function sendRequest(RequestInterface $request, array $options = [])
+    public function sendRequest(RequestInterface $request)
     {
-        $options = $this->buildOptions($options);
-
         try {
-            return $this->client->send($request, $options);
+            return $this->client->send($request, $this->options);
         } catch (RequestException $e) {
             throw $this->createException($e);
         }
@@ -53,56 +63,42 @@ class Guzzle6HttpAdapter implements HttpAdapter
     /**
      * {@inheritdoc}
      */
-    public function sendRequests(array $requests, array $options = [])
+    public function sendRequests(array $requests)
     {
-        $options = $this->buildOptions($options);
+        $poolResult  = Pool::batch($this->client, $requests, ['options' => $this->options]);
+        $batchResult = new BatchResult();
 
-        $results = Pool::batch($this->client, $requests, $options);
-
-        $exceptions = [];
-        $responses = [];
-
-        foreach ($results as $result) {
+        foreach ($poolResult as $index => $result) {
             if ($result instanceof ResponseInterface) {
-                $responses[] = $result;
-            } elseif ($result instanceof RequestException) {
-                $exceptions[] = $this->createException($result);
+                $batchResult = $batchResult->addResponse($requests[$index], $result);
+            }
+
+            if ($result instanceof RequestException) {
+                $batchResult = $batchResult->addException($requests[$index], $this->createException($result));
             }
         }
 
-        if (count($exceptions) > 0) {
-            throw new Exception\MultiHttpAdapterException($exceptions, $responses);
+        if ($batchResult->hasExceptions()) {
+            throw new BatchException($batchResult);
         }
 
-        return $results;
+        return $batchResult;
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getName()
-    {
-        return 'guzzle6';
-    }
-
-    /**
-     * Converts a Guzzle exception into an HttpAdapter exception
+     * Converts a Guzzle exception into an Httplug exception
      *
      * @param RequestException $exception
      *
-     * @return Exception\HttpAdapterException
+     * @return HttpException|NetworkException Return an HttpException if response is available, NetworkException otherwise
      */
     private function createException(RequestException $exception)
     {
-        $adapterException = new Exception\HttpAdapterException(
-            $exception->getMessage(),
-            0,
-            $exception
-        );
-        $adapterException->setResponse($exception->getResponse());
-        $adapterException->setRequest($exception->getRequest());
+        if ($exception->hasResponse()) {
+            return new HttpException($exception->getMessage(), $exception->getRequest(), $exception->getResponse(), $exception);
+        }
 
-        return $adapterException;
+        return new NetworkException($exception->getMessage(), $exception->getRequest(), $exception);
     }
 
     /**
